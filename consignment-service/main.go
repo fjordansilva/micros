96819f29218056c2b5501f79b7b39da93278930c
Fernando Jordan Silva <fjordansilva@gmail.com>
2018-10-30 10:26:18 +0100

    Servicio Vessel

--- consignment-service/main.go
+++ consignment-service/main.go
@@ -5,40 +5,57 @@ import (
 	// Import the generated protobuf code
 	"context"
 	pb "github.com/fjordansilva/micros/consignment-service/proto/consignment"
+	vesselProto "github.com/fjordansilva/micros/vessel-service/proto/vessel"
 	"github.com/micro/go-micro"
 	"log"
 )
 
-type IRepository interface {
+type Repository interface {
 	Create(*pb.Consignment) (*pb.Consignment, error)
 	GetAll() []*pb.Consignment
 }
 
 // Repositorio - Dummy repository. Simula el uso de un datastore
 // Se cambiara con una implementacion real
-type Repository struct {
+type ConsignmentRepository struct {
 	consignments []*pb.Consignment
 }
 
-func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
+func (repo *ConsignmentRepository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
 	updated := append(repo.consignments, consignment)
 	repo.consignments = updated
 	return consignment, nil
 }
 
-func (repo *Repository) GetAll() []*pb.Consignment {
+func (repo *ConsignmentRepository) GetAll() []*pb.Consignment {
 	return repo.consignments
 }
 
 // El servicio debe implementar todos los metodos para satisfacer la definicion realizada en el fichero protobuf.
 // Se puede comprobar la interfaz en el fichero autogenerado.
 type service struct {
-	repo IRepository
+	repo Repository
+	vesselClient vesselProto.VesselServiceClient
 }
 
 // CreateConsigment - creamos solo un metodo para el servicio
 // Este metodo obtiene un contexto y una request como argumento que se envia al servidor gRPC.
 func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
+	// Here we call a client instance of our vessel service with out consignment weight, and the amount of
+	// containers as the capacity value
+	vesselResponse, err := s.vesselClient.FindAvailable(context.Background(), &vesselProto.Specification{
+		MaxWeight: req.Weight,
+		Capacity: int32(len(req.Containers)),
+	})
+	log.Printf("Found vessel: %s \n", vesselResponse.Vessel.Name)
+	if err != nil {
+		return err
+	}
+
+	// We set the vesselId as the vessel we got back from our vessel service
+	req.VesselId = vesselResponse.Vessel.Id
+
+	// Save our consignment
 	consignment, err := s.repo.Create(req)
 	if err != nil {
 		return err
@@ -57,7 +74,7 @@ func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest, res *
 }
 
 func main() {
-	repo := &Repository{}
+	repo := &ConsignmentRepository{}
 
 	// Create a new service. Optionally include some options here.
 	srv := micro.NewService(
@@ -66,11 +83,13 @@ func main() {
 		micro.Version("latest"),
 	)
 
+	vesselClient := vesselProto.NewVesselServiceClient("go.micro.srv.vessel", srv.Client())
+
 	// Init will parse the command line flags
 	srv.Init()
 
 	// Register handler
-	pb.RegisterShippingServiceHandler(srv.Server(), &service{repo})
+	pb.RegisterShippingServiceHandler(srv.Server(), &service{repo, vesselClient})
 
 	// Run the server
 	if err := srv.Run(); err != nil {
--- vessel-service/main.go
+++ vessel-service/main.go
@@ -1,13 +1,68 @@
+// vessel-service/main.go
 package main
 
 import (
+	"context"
 	pb "github.com/fjordansilva/micros/vessel-service/proto/vessel"
+	"github.com/micro/go-micro"
+	"github.com/micro/go-micro/errors"
+	"log"
 )
 
-type Repository {
-	FindAvailable(*pb.Specification)
+type Repository interface {
+	FindAvailable(*pb.Specification) (*pb.Vessel, error)
+}
+
+type VesselRepository struct {
+	vessels []*pb.Vessel
+}
+
+// FindAvailable - comprueba una especificacion contra un mapa de Vessels
+// Si la capacidad y el peso maximo estan por debajo de la capacidad del vessel y su peso maximo,
+// se devuelve ese vessel
+func (repo *VesselRepository) FindAvailable(spec *pb.Specification) (*pb.Vessel, error)  {
+	for _, vessel := range repo.vessels {
+		if spec.Capacity <= vessel.Capacity && spec.MaxWeight <= vessel.MaxWeight {
+			return vessel, nil
+		}
+	}
+	return nil, errors.New(1,"No vessel found by that spec", 1)
+}
+
+// gRPC service handler
+type service struct {
+	repo Repository
+}
+
+func (s *service) FindAvailable(ctx context.Context, req *pb.Specification, res *pb.Response) error  {
+	// Find the next available vessel
+	vessel, err := s.repo.FindAvailable(req)
+	if err != nil {
+		return err
+	}
+
+	// Set the vessel as part of the response message type
+	res.Vessel = vessel
+	return nil
 }
 
 func main() {
-	
+	vessels := []*pb.Vessel {
+		&pb.Vessel{Id: "vessel01", Name: "Boaty McBoatface", MaxWeight: 200000, Capacity: 500},
+	}
+	repo := &VesselRepository{vessels:vessels}
+
+	srv := micro.NewService(
+		micro.Name("go.micro.srv.vessel"),
+		micro.Version("latest"),
+	)
+
+	srv.Init()
+
+	// Register our implementarion with
+	pb.RegisterVesselServiceHandler(srv.Server(), &service{repo})
+
+	if err := srv.Run(); err != nil {
+		log.Fatal(err)
+	}
 }
